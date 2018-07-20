a329da13d06734c57d4e5bd94dcc7af11a848182
Mr.Soft <trongp9@gmail.com>
2018-07-20 20:38:40 +0700

    trong commit

--- Lesson01/ForStudent/HuyTrong_layoutproject/LinearLayout/app/src/main/java/com/phamhuytrong/linearlayout/MainActivity.java
+++ Lesson01/ForStudent/HuyTrong_layoutproject/LinearLayout/app/src/main/java/com/phamhuytrong/linearlayout/MainActivity.java
@@ -2,12 +2,14 @@ package com.phamhuytrong.linearlayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.relativelayout_login);
+
     }
 }
